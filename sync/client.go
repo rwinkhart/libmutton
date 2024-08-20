rwinkhart/libmutton

commit 88b7a6be6d915aeee1b3e4ba52248a428e41bc3c

Create all config/entry files (both local and remote) with 0600 permissions

sync/client.go
@@ -53,7 +53,7 @@ func GetSSHClient(manualSync bool) (*ssh.Client, string, bool) {
 		case 6:
 			isWindows, err = strconv.ParseBool(key)
 			if err != nil {
-				fmt.Println(core.AnsiError + "Sync failed - Unable to parse server OS type: " + err.Error() + core.AnsiReset)
+				fmt.Println(core.AnsiError+"Sync failed - Unable to parse server OS type:", err.Error()+core.AnsiReset)
 				os.Exit(101)
 			}
 		}
@@ -82,7 +82,7 @@ func GetSSHClient(manualSync bool) (*ssh.Client, string, bool) {
 	var hostKeyCallback ssh.HostKeyCallback
 	hostKeyCallback, err = knownhosts.New(core.Home + core.PathSeparator + ".ssh" + core.PathSeparator + "known_hosts")
 	if err != nil {
-		fmt.Println(core.AnsiError + "Sync failed - Unable to read known hosts file: " + err.Error() + core.AnsiReset)
+		fmt.Println(core.AnsiError+"Sync failed - Unable to read known hosts file:", err.Error()+core.AnsiReset)
 		os.Exit(101)
 	}
 
@@ -157,7 +157,7 @@ func getRemoteDataFromClient(sshClient *ssh.Client, manualSync bool) (map[string
 	}
 	serverTime, err := strconv.ParseInt(outputSlice[0], 10, 64)
 	if err != nil {
-		fmt.Println(core.AnsiError+"Sync failed - Unable to parse server time: ", err.Error()+core.AnsiReset)
+		fmt.Println(core.AnsiError+"Sync failed - Unable to parse server time:", err.Error()+core.AnsiReset)
 		os.Exit(101)
 	}
 	entries := strings.Split(outputSlice[1], FSMisc)[1:]
@@ -171,7 +171,7 @@ func getRemoteDataFromClient(sshClient *ssh.Client, manualSync bool) (map[string
 	for _, modString := range modsStrings {
 		mod, err = strconv.ParseInt(modString, 10, 64)
 		if err != nil {
-			fmt.Println(core.AnsiError+"Sync failed - Unable to parse mod time: ", err.Error()+core.AnsiReset)
+			fmt.Println(core.AnsiError+"Sync failed - Unable to parse mod time:", err.Error()+core.AnsiReset)
 			os.Exit(101)
 		}
 		mods = append(mods, mod)
@@ -224,7 +224,7 @@ func sftpSync(sshClient *ssh.Client, sshEntryRoot string, sshIsWindows bool, dow
 	defer func(sftpClient *sftp.Client) {
 		err = sftpClient.Close()
 		if err != nil {
-			fmt.Println(core.AnsiError+"Sync failed - Unable to close SFTP client: ", err.Error()+core.AnsiReset)
+			fmt.Println(core.AnsiError+"Sync failed - Unable to close SFTP client:", err.Error()+core.AnsiReset)
 			os.Exit(104)
 		}
 	}(sftpClient)
@@ -261,7 +261,7 @@ func sftpSync(sshClient *ssh.Client, sshEntryRoot string, sshIsWindows bool, dow
 
 		// create local file
 		var localFile *os.File
-		localFile, err = os.Create(localEntryFullPath)
+		localFile, err = os.OpenFile(localEntryFullPath, os.O_CREATE|os.O_WRONLY, 0600)
 		if err != nil {
 			fmt.Println(core.AnsiError+"Sync failed - Unable to create local file:", err.Error()+core.AnsiReset)
 			os.Exit(102)
@@ -318,7 +318,7 @@ func sftpSync(sshClient *ssh.Client, sshEntryRoot string, sshIsWindows bool, dow
 
 		// create remote file
 		var remoteFile *sftp.File
-		remoteFile, err = sftpClient.Create(remoteEntryFullPath)
+		remoteFile, err = sftpClient.OpenFile(remoteEntryFullPath, os.O_CREATE|os.O_WRONLY)
 		if err != nil {
 			fmt.Println(core.AnsiError+"Sync failed - Unable to create remote file ("+remoteEntryFullPath+"):", err.Error()+core.AnsiReset)
 			os.Exit(102)
@@ -334,6 +334,13 @@ func sftpSync(sshClient *ssh.Client, sshEntryRoot string, sshIsWindows bool, dow
 		// close the files
 		_ = localFile.Close() // errors ignored; if the files could be opened/created, it can probably be closed
 		_ = remoteFile.Close()
+
+		// set permissions on remote file
+		err = sftpClient.Chmod(remoteEntryFullPath, 0600)
+		if err != nil {
+			fmt.Println(core.AnsiError+"Sync failed - Unable to set permissions on remote file:", err.Error()+core.AnsiReset)
+			os.Exit(103)
+		}
 
 		// set the modification time of the remote file to match the value saved from the local file (from before the upload)
 		err = sftpClient.Chtimes(remoteEntryFullPath, time.Now(), modTime)
@@ -396,7 +403,7 @@ func deletionSync(deletions []string) {
 		fmt.Println(ansiDelete+deletion+core.AnsiReset, "has been sheared, removing locally (if it exists)")
 		err := os.RemoveAll(core.TargetLocationFormat(deletion))
 		if err != nil {
-			fmt.Println(core.AnsiError + "Sync failed - Failed to shear " + deletion + " locally: " + err.Error() + core.AnsiReset)
+			fmt.Println(core.AnsiError+"Sync failed - Failed to shear "+deletion+" locally:", err.Error()+core.AnsiReset)
 			os.Exit(102)
 		}
 	}
@@ -457,7 +464,7 @@ func folderSync(folders []string) {
 		if !isFile && !isAccessible {
 			err := os.MkdirAll(folderFullPath, 0700)
 			if err != nil {
-				fmt.Println(core.AnsiError + "Sync failed - Failed to create folder \"" + folder + "\": " + err.Error() + core.AnsiReset)
+				fmt.Println(core.AnsiError+"Sync failed - Failed to create folder \""+folder+"\":", err.Error()+core.AnsiReset)
 				os.Exit(102)
 			}
 		} else if isFile {
@@ -475,7 +482,7 @@ func RunJob(manualSync bool) {
 	defer func(sshClient *ssh.Client) {
 		err := sshClient.Close()
 		if err != nil {
-			fmt.Println(core.AnsiError+"Sync failed - Unable to close SSH client: ", err.Error()+core.AnsiReset)
+			fmt.Println(core.AnsiError+"Sync failed - Unable to close SSH client:", err.Error()+core.AnsiReset)
 			os.Exit(104)
 		}
 	}(sshClient)

core/configParser.go
@@ -13,7 +13,7 @@ import (
 func loadConfig() *ini.File {
 	cfg, err := ini.Load(ConfigPath)
 	if err != nil {
-		fmt.Println(AnsiError + "Failed to load libmutton.ini: " + err.Error() + AnsiReset)
+		fmt.Println(AnsiError+"Failed to load libmutton.ini:", err.Error()+AnsiReset)
 		os.Exit(101)
 	}
 	return cfg
@@ -56,7 +56,7 @@ func GenDeviceIDList(errorOnFail bool) *[]fs.DirEntry {
 	// create a slice of all registered devices
 	deviceIDList, err := os.ReadDir(ConfigDir + PathSeparator + "devices")
 	if err != nil && errorOnFail {
-		fmt.Println(AnsiError + "Failed to read the devices directory: " + err.Error() + AnsiReset)
+		fmt.Println(AnsiError+"Failed to read the devices directory:", err.Error()+AnsiReset)
 		os.Exit(101)
 	}
 	return &deviceIDList
@@ -93,7 +93,7 @@ func WriteConfig(valuesToWrite [][3]string, append bool) {
 	// save to libmutton.ini
 	err := cfg.SaveTo(ConfigPath)
 	if err != nil {
-		fmt.Println(AnsiError + "Failed to save libmutton.ini: " + err.Error() + AnsiReset)
+		fmt.Println(AnsiError+"Failed to save libmutton.ini:", err.Error()+AnsiReset)
 		os.Exit(102)
 	}
 }

core/copyDARWIN.go
@@ -16,7 +16,7 @@ func copyField(executableName, copySubject string) {
 	writeToStdin(cmd, copySubject)
 	err := cmd.Run()
 	if err != nil {
-		fmt.Println(AnsiError + "Failed to copy to clipboard: " + err.Error() + AnsiReset)
+		fmt.Println(AnsiError+"Failed to copy to clipboard:", err.Error()+AnsiReset)
 		os.Exit(110)
 	}
 
@@ -40,7 +40,7 @@ func clipClear(oldContents string) {
 	cmd := exec.Command("pbpaste")
 	newContents, err := cmd.Output()
 	if err != nil {
-		fmt.Println(AnsiError + "Failed to read clipboard contents: " + err.Error() + AnsiReset)
+		fmt.Println(AnsiError+"Failed to read clipboard contents:", err.Error()+AnsiReset)
 		os.Exit(110)
 	}
 
@@ -49,7 +49,7 @@ func clipClear(oldContents string) {
 		writeToStdin(cmd, "")
 		err = cmd.Run()
 		if err != nil {
-			fmt.Println(AnsiError + "Failed to clear clipboard: " + err.Error() + AnsiReset)
+			fmt.Println(AnsiError+"Failed to clear clipboard:", err.Error()+AnsiReset)
 			os.Exit(110)
 		}
 	}

core/copyTERMUX.go
@@ -16,7 +16,7 @@ func copyField(executableName, copySubject string) {
 	writeToStdin(cmd, copySubject)
 	err := cmd.Run()
 	if err != nil {
-		fmt.Println(AnsiError + "Failed to copy to clipboard: " + err.Error() + AnsiReset)
+		fmt.Println(AnsiError+"Failed to copy to clipboard:", err.Error()+AnsiReset)
 		os.Exit(110)
 	}
 
@@ -40,7 +40,7 @@ func clipClear(oldContents string) {
 	cmd := exec.Command("termux-clipboard-get")
 	newContents, err := cmd.Output()
 	if err != nil {
-		fmt.Println(AnsiError + "Failed to read clipboard contents: " + err.Error() + AnsiReset)
+		fmt.Println(AnsiError+"Failed to read clipboard contents:", err.Error()+AnsiReset)
 		os.Exit(110)
 	}
 
@@ -49,7 +49,7 @@ func clipClear(oldContents string) {
 		writeToStdin(cmd, "")
 		err = cmd.Run()
 		if err != nil {
-			fmt.Println(AnsiError + "Failed to clear clipboard: " + err.Error() + AnsiReset)
+			fmt.Println(AnsiError+"Failed to clear clipboard:", err.Error()+AnsiReset)
 			os.Exit(110)
 		}
 	}

core/copyUNIXGeneric.go
@@ -27,7 +27,7 @@ func copyField(executableName, copySubject string) {
 	writeToStdin(cmd, copySubject)
 	err := cmd.Run()
 	if err != nil {
-		fmt.Println(AnsiError + "Failed to copy to clipboard: " + err.Error() + AnsiReset)
+		fmt.Println(AnsiError+"Failed to copy to clipboard:", err.Error()+AnsiReset)
 		os.Exit(110)
 	}
 
@@ -65,7 +65,7 @@ func clipClear(oldContents string) {
 	// read current clipboard contents
 	newContents, err := cmdPaste.Output()
 	if err != nil {
-		fmt.Println(AnsiError + "Failed to read clipboard contents: " + err.Error() + AnsiReset)
+		fmt.Println(AnsiError+"Failed to read clipboard contents:", err.Error()+AnsiReset)
 		os.Exit(110)
 	}
 
@@ -73,7 +73,7 @@ func clipClear(oldContents string) {
 	if oldContents == strings.TrimRight(string(newContents), "\r\n") {
 		err = cmdClear.Run()
 		if err != nil {
-			fmt.Println(AnsiError + "Failed to clear clipboard: " + err.Error() + AnsiReset)
+			fmt.Println(AnsiError+"Failed to clear clipboard:", err.Error()+AnsiReset)
 			os.Exit(110)
 		}
 	}

core/copyWIN.go
@@ -15,7 +15,7 @@ func copyField(executableName, copySubject string) {
 	cmd := exec.Command("powershell.exe", "-c", fmt.Sprintf("echo '%s' | Set-Clipboard", strings.ReplaceAll(copySubject, "'", "''")))
 	err := cmd.Run()
 	if err != nil {
-		fmt.Println(AnsiError + "Failed to copy to clipboard: " + err.Error() + AnsiReset)
+		fmt.Println(AnsiError+"Failed to copy to clipboard:", err.Error()+AnsiReset)
 		os.Exit(110)
 	}
 
@@ -39,7 +39,7 @@ func clipClear(oldContents string) {
 	cmd := exec.Command("powershell.exe", "-c", "Get-Clipboard")
 	newContents, err := cmd.Output()
 	if err != nil {
-		fmt.Println(AnsiError + "Failed to read clipboard contents: " + err.Error() + AnsiReset)
+		fmt.Println(AnsiError+"Failed to read clipboard contents:", err.Error()+AnsiReset)
 		os.Exit(110)
 	}
 
@@ -47,7 +47,7 @@ func clipClear(oldContents string) {
 		cmd = exec.Command("powershell.exe", "-c", "Set-Clipboard")
 		err = cmd.Run()
 		if err != nil {
-			fmt.Println(AnsiError + "Failed to clear clipboard: " + err.Error() + AnsiReset)
+			fmt.Println(AnsiError+"Failed to clear clipboard:", err.Error()+AnsiReset)
 			os.Exit(110)
 		}
 	}

core/init.go
@@ -45,7 +45,7 @@ func GpgKeyGen() string {
 	cmd.Stdin = os.Stdin
 	err := cmd.Run()
 	if err != nil {
-		fmt.Println(AnsiError + "Failed to generate GPG key: " + err.Error() + AnsiReset)
+		fmt.Println(AnsiError+"Failed to generate GPG key:", err.Error()+AnsiReset)
 		os.Exit(111)
 	}
 
@@ -58,7 +58,7 @@ func DirInit(preserveOldConfigDir bool) string {
 	// create EntryRoot
 	err := os.MkdirAll(EntryRoot, 0700)
 	if err != nil {
-		fmt.Println(AnsiError + "Failed to create \"" + EntryRoot + "\": " + err.Error() + AnsiReset)
+		fmt.Println(AnsiError+"Failed to create \""+EntryRoot+"\":", err.Error()+AnsiReset)
 		os.Exit(102)
 	}
 
@@ -71,7 +71,7 @@ func DirInit(preserveOldConfigDir bool) string {
 		if isAccessible {
 			err = os.RemoveAll(ConfigDir)
 			if err != nil {
-				fmt.Println(AnsiError + "Failed to remove existing config directory: " + err.Error() + AnsiReset)
+				fmt.Println(AnsiError+"Failed to remove existing config directory:", err.Error()+AnsiReset)
 				os.Exit(102)
 			}
 		}
@@ -80,7 +80,7 @@ func DirInit(preserveOldConfigDir bool) string {
 	// create config directory w/devices subdirectory
 	err = os.MkdirAll(ConfigDir+PathSeparator+"devices", 0700)
 	if err != nil {
-		fmt.Println(AnsiError + "Failed to create \"" + ConfigDir + "\": " + err.Error() + AnsiReset)
+		fmt.Println(AnsiError+"Failed to create \""+ConfigDir+"\":", err.Error()+AnsiReset)
 		os.Exit(102)
 	}
 

core/utilitiesMisc.go
@@ -52,7 +52,7 @@ func WriteEntry(targetLocation string, entryData []string, verifyEntryDoesNotExi
 	encryptedBytes := EncryptGPG(entryData)
 	err := os.WriteFile(targetLocation, encryptedBytes, 0600)
 	if err != nil {
-		fmt.Println(AnsiError + "Failed to write to file: " + err.Error() + AnsiReset)
+		fmt.Println(AnsiError+"Failed to write to file:", err.Error()+AnsiReset)
 		os.Exit(102)
 	}
 }
@@ -61,7 +61,7 @@ func WriteEntry(targetLocation string, entryData []string, verifyEntryDoesNotExi
 func writeToStdin(cmd *exec.Cmd, input string) {
 	stdin, err := cmd.StdinPipe()
 	if err != nil {
-		fmt.Println(AnsiError + "Failed to access stdin for system command: " + err.Error() + AnsiReset)
+		fmt.Println(AnsiError+"Failed to access stdin for system command:", err.Error()+AnsiReset)
 		os.Exit(111)
 	}
 
@@ -77,7 +77,7 @@ func writeToStdin(cmd *exec.Cmd, input string) {
 func CreateTempFile() *os.File {
 	tempFile, err := os.CreateTemp("", "*.markdown")
 	if err != nil {
-		fmt.Println(AnsiError + "Failed to create temporary file: " + err.Error() + AnsiReset)
+		fmt.Println(AnsiError+"Failed to create temporary file:", err.Error()+AnsiReset)
 		os.Exit(102)
 	}
 	return tempFile

commit de508d17f2b8bebfe895df8c81a87aa058bb5313

Fix edited entries (edited to have fewer lines than before) not being truncated during sync (regression from 88b7a6be6d915aeee1b3e4ba52248a428e41bc3c)

sync/client.go
@@ -261,7 +261,7 @@ func sftpSync(sshClient *ssh.Client, sshEntryRoot string, sshIsWindows bool, dow
 
 		// create local file
 		var localFile *os.File
-		localFile, err = os.OpenFile(localEntryFullPath, os.O_CREATE|os.O_WRONLY, 0600)
+		localFile, err = os.OpenFile(localEntryFullPath, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0600)
 		if err != nil {
 			fmt.Println(core.AnsiError+"Sync failed - Unable to create local file:", err.Error()+core.AnsiReset)
 			os.Exit(core.ErrorWrite)
@@ -318,7 +318,7 @@ func sftpSync(sshClient *ssh.Client, sshEntryRoot string, sshIsWindows bool, dow
 
 		// create remote file
 		var remoteFile *sftp.File
-		remoteFile, err = sftpClient.OpenFile(remoteEntryFullPath, os.O_CREATE|os.O_WRONLY)
+		remoteFile, err = sftpClient.OpenFile(remoteEntryFullPath, os.O_CREATE|os.O_TRUNC|os.O_WRONLY)
 		if err != nil {
 			fmt.Println(core.AnsiError+"Sync failed - Unable to create remote file ("+remoteEntryFullPath+"):", err.Error()+core.AnsiReset)
 			os.Exit(core.ErrorWrite)