4ca97834f44f710dcbff6c67af98d02d9729cebb
Randall Winkhart <idgr@tutanota.com>
2025-01-11 18:11:27 -0500

    Fix formatting of copyright date in version info output

--- libmuttonserver.go
+++ libmuttonserver.go
@@ -116,7 +116,5 @@ ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
            OR OTHER DEALINGS IN THE SOFTWARE.` + "\n\n---------------------------------------------------------")
-	fmt.Print(ansiBold + "\n\n              libmuttonserver" + core.AnsiReset + " Version " + core.LibmuttonVersion + `
-
-           Copyright (c) 2024-2025 Randall Winkhart` + "\n\n")
+	fmt.Print(ansiBold + "\n\n              libmuttonserver" + core.AnsiReset + " Version " + core.LibmuttonVersion + "\n\n        Copyright (c) 2024-2025: Randall Winkhart" + "\n\n")
 }
