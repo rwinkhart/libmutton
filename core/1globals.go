826d94bc4397322ca7f64b89203f0d605e56148f
Randall Winkhart <idgr@tutanota.com>
2025-05-04 18:05:09 -0400

    Bump version to v0.E.0 (WIP v0.4.0)

--- core/1globals.go
+++ core/1globals.go
@@ -12,7 +12,7 @@ var (
 )
 
 const (
-	LibmuttonVersion = "0.3.1" // Untagged releases feature a letter suffix corresponding to the eventual release version, e.g "0.2.A" -> "0.2.0", "0.2.B" -> "0.2.1"
+	LibmuttonVersion = "0.E.0" // Untagged releases feature a letter suffix corresponding to the eventual release version, e.g "0.2.A" -> "0.2.0", "0.2.B" -> "0.2.1"
 
 	FSSpace = "\u259d" // ▝ Space/list separator
 	FSPath  = "\u259e" // ▞ Path separator
