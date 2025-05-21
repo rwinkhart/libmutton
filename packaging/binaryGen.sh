3c5db78da9861e09112ac308c086c2d19f2abfeb
Randall Winkhart <idgr@tutanota.com>
2025-05-21 16:40:26 +0000

    Build release binaries to target x86_64_v2 and arm64v8.7 (v8.0 still offered on Linux)

--- packaging/binaryGen.sh
+++ packaging/binaryGen.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 # This script generates portable libmuttonserver release binaries for the following platforms:
-# - Linux (x86_64_v1)
-# - Linux (aarch64)
-# - Windows (x86_64_v1)
-# - Windows (aarch64)
+# - Linux (x86_64_v2)
+# - Linux (arm64v8.0)
+# - Linux (arm64v8.7)
+# - Windows (x86_64_v2)
+# - Windows (arm64v8.7)
 
 mkdir -p ./1output
 cd ..
-GOOS=linux CGO_ENABLED=0 GOAMD64=v1 go build -o ./packaging/1output/libmuttonserver-linux-x86_64_v1 -ldflags="-s -w" -trimpath ./libmuttonserver.go
-GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -o ./packaging/1output/libmuttonserver-linux-aarch64 -ldflags="-s -w" -trimpath ./libmuttonserver.go
-GOOS=windows CGO_ENABLED=0 GOAMD64=v1 go build -o ./packaging/1output/libmuttonserver-windows-x86_64_v1.exe -ldflags="-s -w" -trimpath ./libmuttonserver.go
-GOOS=windows GOARCH=arm64 CGO_ENABLED=0 go build -o ./packaging/1output/libmuttonserver-windows-aarch64.exe -ldflags="-s -w" -trimpath ./libmuttonserver.go
+GOOS=linux CGO_ENABLED=0 GOAMD64=v2 go build -o ./packaging/1output/libmuttonserver-linux-x86_64_v2 -ldflags="-s -w" -trimpath ./libmuttonserver.go
+GOOS=linux GOARCH=arm64 GOARM64=v8.0 CGO_ENABLED=0 go build -o ./packaging/1output/libmuttonserver-linux-arm64v8.0.exe -ldflags="-s -w" -trimpath ./libmuttonserver.go
+GOOS=linux GOARCH=arm64 GOARM64=v8.7 CGO_ENABLED=0 go build -o ./packaging/1output/libmuttonserver-linux-arm64v8.7.exe -ldflags="-s -w" -trimpath ./libmuttonserver.go
+GOOS=windows CGO_ENABLED=0 GOAMD64=v2 go build -o ./packaging/1output/libmuttonserver-windows-x86_64_v2.exe -ldflags="-s -w" -trimpath ./libmuttonserver.go
+GOOS=windows GOARCH=arm64 GOARM64=v8.7 CGO_ENABLED=0 go build -o ./packaging/1output/libmuttonserver-windows-arm64v8.7.exe -ldflags="-s -w" -trimpath ./libmuttonserver.go
