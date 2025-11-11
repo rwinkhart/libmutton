6d1513b469081290c0b32727c3e15233c6dff92b
Randall Winkhart <idgr@tutanota.com>
2025-11-11 01:01:28 -0500

    Fix 8f4590cf341a598761e3b3087bb242fc20aeb678 on Windows

--- global/sysProcAttr_WIN.go
+++ global/sysProcAttr_WIN.go
@@ -5,5 +5,5 @@ package global
 import "syscall"
 
 func GetSysProcAttr() *syscall.SysProcAttr {
-	return &syscall.SysProcAttr{}
+	return &syscall.SysProcAttr{CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP}
 }
