941a2dc8b80cb6e0327b33714c4a894962621bda
Randall Winkhart <idgr@tutanota.com>
2025-12-11 19:31:37 -0500

    Eliminate synccycles package [synccycles.DeviceIDGen() => syncclient.DeviceIDGenFromClient()]

--- core/init.go
+++ core/init.go
@@ -9,7 +9,7 @@ import (
 	"github.com/rwinkhart/go-boilerplate/back"
 	"github.com/rwinkhart/libmutton/cfg"
 	"github.com/rwinkhart/libmutton/global"
-	"github.com/rwinkhart/libmutton/synccycles"
+	"github.com/rwinkhart/libmutton/syncclient"
 	"github.com/rwinkhart/rcw/wrappers"
 )
 
@@ -80,7 +80,7 @@ func LibmuttonInit(inputCB func(prompt string) string, clientSpecificIniData [][
 			return errors.New("unable to write config file: " + err.Error())
 		}
 		// generate and register device ID
-		sshEntryRoot, sshAgeDir, sshIsWindows, err := synccycles.DeviceIDGen(oldDeviceID, "")
+		sshEntryRoot, sshAgeDir, sshIsWindows, err := syncclient.DeviceIDGenFromClient(oldDeviceID, "")
 		if err != nil {
 			return errors.New("unable to generate device ID: " + err.Error())
 		}
