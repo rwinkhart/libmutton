e9f49fb98aa28d866014a599ac1a67e118d67a03
Randall Winkhart <idgr@tutanota.com>
2026-02-17 20:10:24 -0500

    Fix cmd pop-up on background clipclear on Windows

--- clip/copy_WIN.go
+++ clip/copy_WIN.go
@@ -5,9 +5,11 @@ package clip
 import (
 	"errors"
 	"os/exec"
+	"syscall"
 
 	"github.com/rwinkhart/go-boilerplate/back"
 	"github.com/rwinkhart/go-boilerplate/security"
+	"golang.org/x/sys/windows"
 )
 
 // CopyBytes copies a byte slice to the clipboard.
@@ -28,7 +30,11 @@ func CopyBytes(clearClipboardAutomatically bool, copySubject []byte) error {
 
 // getClipCommands returns the commands for pasting and clearing the clipboard contents.
 func getClipCommands() (*exec.Cmd, *exec.Cmd, error) {
+	sysProcAttr := &syscall.SysProcAttr{CreationFlags: windows.CREATE_NO_WINDOW}
+	pasteCMD := exec.Command("powershell.exe", "-c", "Get-Clipboard")
+	pasteCMD.SysProcAttr = sysProcAttr
 	clearCMD := exec.Command("clip.exe")
+	clearCMD.SysProcAttr = sysProcAttr
 	_ = back.WriteToStdin(clearCMD, nil, false)
-	return exec.Command("powershell.exe", "-c", "Get-Clipboard"), clearCMD, nil
+	return pasteCMD, clearCMD, nil
 }
